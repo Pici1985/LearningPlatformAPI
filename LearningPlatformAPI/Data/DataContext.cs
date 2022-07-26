2ef9bcf97a46cb99f9f77ee15af5f21aaa51454b
Balazs <balazs@frog.net>
2022-07-26 10:29:31 +0100

    Add project files.

---
+++ LearningPlatformAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using LearningPlatformAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningPlatformAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ LearningPlatformAPI/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningPlatformAPI.Models
+{
+    public class Person
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }    
+        public string Email { get; set; }
+        public string Password { get; set; }    
+        public string Gender { get; set; }  
+        public int Age { get; set; }    
+        public string Occupation { get; set; } 
+        public DateTime DateOfRegistration { get; set; }
+
+    }
+}
